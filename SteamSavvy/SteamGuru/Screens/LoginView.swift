e5e6a4d0f132e59b16e02163311f0242858fcdd3
Gabe Grassi <131699222+ggrassi09@users.noreply.github.com>
2024-03-26 21:43:15 -0400

    Change authentication func to use login API

--- SteamSavvy/SteamGuru/Screens/LoginView.swift
+++ SteamSavvy/SteamGuru/Screens/LoginView.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import Observation
+
 
 struct LoginView: View {
     @State private var username = ""
     @State private var password = ""
-    @State private var wrongUsername = 0
-    @State private var wrongPassword = 0
+    @State private var wrongInput = 0
+    
     @State private var showingLoginScreen = false
     
     var body: some View {
@@ -35,14 +37,21 @@ struct LoginView: View {
                         .frame(width: 300, height: 50)
                         .background(Color.white.opacity(0.3))
                         .cornerRadius(10)
-                        .border(.red, width: CGFloat(wrongUsername)).cornerRadius(4)
+                        //.border(.red, width: CGFloat(wrongUsername)).cornerRadius(4)
                     
                     SecureField("Password", text: $password)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.white.opacity(0.3))
                         .cornerRadius(10)
-                        .border(.red, width: CGFloat(wrongPassword)).cornerRadius(4)
+                        //.border(.red, width: CGFloat(wrongPassword)).cornerRadius(4)
+                    
+                    if(wrongInput == 2){
+                        Text("Incorrect Username/Password")
+                            .font(.callout)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color(hue: 1.0, saturation: 0.777, brightness: 0.831))
+                    }
                     
                     Button(action: {doLogin(login: username, password: password)}){
                         //User authentication logic would go here, using test values right now
@@ -54,7 +63,6 @@ struct LoginView: View {
                             .background(Color.blue.opacity(0.8))
                             .cornerRadius(10)
                     }
-                   
                     
                     NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen){
                         EmptyView()
@@ -66,19 +74,80 @@ struct LoginView: View {
         }
     }
     
-    func autheticateUser(username: String, password: String){
-        if username.lowercased() == "rickl" {
-            wrongUsername = 0
-            if password == "COP4331" {
-                wrongPassword = 0
-                showingLoginScreen = true
-            } else {
-                wrongPassword = 2
-            }
-        } else {
-            wrongUsername = 2
+    func doLogin(login: String, password: String){
+        let loginEndpoint: String = "https://steamguru-77d4152ed074.herokuapp.com/api/login"
+        guard let loginURL = URL(string: loginEndpoint) else {
+            print("Error: cannot create URL")
+            return
         }
+        var loginUrlRequest = URLRequest(url: loginURL)
+        loginUrlRequest.httpMethod = "POST"
+        
+        let parameters: [String: Any] = [
+            "login": login,
+            "password": password
+        
+        ]
+        
+        let jsonLogin: Data
+        do {
+            jsonLogin = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            loginUrlRequest.httpBody = jsonLogin
+        } catch{
+            print("Error: cannot create JSON from login")
+            return
+        }
+        
+        loginUrlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        loginUrlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: loginUrlRequest){
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling POST on /api/login")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            
+            do {
+                guard let receivedLogin = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] else {
+                    print("Could not get JSON from responseData as dictionary")
+                    return
+                }
+                print("The login is: " + receivedLogin.description)
+                
+                let loginID = receivedLogin["id"] //as? String else {
+                   // print("Could not get loginID as int from JSON")
+                    //return
+               // }
+                print("The ID is: \(loginID ?? 0)")
+                
+                
+                
+                if("\(loginID ?? 0)" > "0"){
+                    print("Sucessful login")
+                    wrongInput = 0
+                    showingLoginScreen = true
+                } else {
+                    wrongInput = 2
+                }
+            } catch {
+                print("error parsing response from POST on /login")
+                return
+            }
+            
+            
+        }
+        task.resume()
     }
+    
+    
 }
 
 #Preview {
