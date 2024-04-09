6aa18b43f02724020295fdab5e440b8a739e8b1e
Gabe Grassi <131699222+ggrassi09@users.noreply.github.com>
2024-04-09 17:01:51 -0400

    Added ImageView
    ImageView is responsible for caching the URLs given by the API and formatting them to display on the screen

---
+++ SteamSavvy/SteamGuru/ImageView.swift
@@ -0,0 +1,81 @@
+//
+//  ImageView.swift
+//  SteamGuru
+//
+//  Created by Gabe Grassi on 4/8/24.
+//
+
+import Foundation
+import SwiftUI
+
+//Used to fetch image from URL and handle the image cache
+class ImageViewModel: ObservableObject {
+    @Published var image: UIImage?
+
+    private var imageCache: NSCache<NSString, UIImage>?
+
+    init(urlString: String?) {
+        loadImage(urlString: urlString)
+    }
+
+    private func loadImage(urlString: String?) {
+        guard let urlString = urlString else { return }
+
+        if let imageFromCache = getImageFromCache(from: urlString) {
+            self.image = imageFromCache
+            return
+        }
+
+        loadImageFromURL(urlString: urlString)
+    }
+
+    private func loadImageFromURL(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                print(error ?? "unknown error")
+                return
+            }
+
+            guard let data = data else {
+                print("No data found")
+                return
+            }
+
+            DispatchQueue.main.async { [weak self] in
+                guard let loadedImage = UIImage(data: data) else { return }
+                self?.image = loadedImage
+                self?.setImageCache(image: loadedImage, key: urlString)
+            }
+        }.resume()
+    }
+
+    private func setImageCache(image: UIImage, key: String) {
+        imageCache?.setObject(image, forKey: key as NSString)
+    }
+
+    private func getImageFromCache(from key: String) -> UIImage? {
+        return imageCache?.object(forKey: key as NSString) as? UIImage
+    }
+}
+
+struct ImageView: View {
+    @ObservedObject private var imageViewModel: ImageViewModel
+    
+    init(urlString: String?) {
+        imageViewModel = ImageViewModel(urlString: urlString)
+    }
+    
+    var body: some View {
+        Image(uiImage: imageViewModel.image ?? UIImage())
+            .resizable()
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        //This is a random image just for preview
+        ImageView(urlString: "https://developer.apple.com/news/images/og/swiftui-og.png")
+    }
+}
