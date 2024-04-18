89e6c2746d0c059c7684dabe03817c8ec590123a
Alyssa Buckingham <lyssmbuckingham@gmail.com>
2024-04-18 16:54:36 -0400

    alyssa updates

--- frontend/src/components/PasswordReset.js
+++ frontend/src/components/PasswordReset.js
@@ -5,32 +5,36 @@ function PasswordReset() {
     var loginName;
     const [message, setMessage] = useState('');
 
-    const doPasswordReset = async event => {
+    const doPasswordReset = async (event) => {
         event.preventDefault();
-        setMessage('TO DO!')
-    }
-    
+        setMessage('TO DO!');
+    };
+
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <form onSubmit={doPasswordReset} style={{ maxWidth: '400px', margin: 'auto' }}>
-                    <h2 className="text-center">Password Reset</h2>
-                    <p style={{ textAlign: 'justify' }}>
-                        Enter the email address associated with the account whose password you'd like to reset.
-                        If there is an account associated with that email address, we will email you a link to reset your password.
-                        Make sure to check your spam!
-                    </p>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="loginName" placeholder="Email" ref={(c) => loginName = c} />
+                <div className="col-md-12">
+                    <div className="card p-3">
+                        <form onSubmit={doPasswordReset}>
+                            <h2 className="text-center">Password Reset</h2>
+                            <p style={{ textAlign: 'justify' }}>
+                                Enter the email address associated with the account whose password you'd like to reset.
+                                If there is an account associated with that email address, we will email you a link to reset your password.
+                                Make sure to check your spam!
+                            </p>
+                            <div className="form-group">
+                                <input type="text" className="form-control" id="loginName" placeholder="Email" ref={(c) => (loginName = c)} />
+                            </div>
+                            <div className="form-group text-center">
+                                <button type="submit" className="btn btn-primary mx-auto d-block">Submit </button>
+                            </div>
+                            <div className="form-group text-center">
+                                <a href="../" className="btn btn-secondary">Return to Login</a>
+                            </div>
+                        </form>
+                        <div id="loginResult" className="red-text">{message}</div>
                     </div>
-                    <div className="form-group text-center">
-                        <button type="submit" className="btn btn-primary mx-auto d-block">Submit </button>
-                    </div>
-                    <div className="form-group text-center">
-                        <a href="../" className="btn btn-primary">Return to Login</a>
-                    </div>
-                </form>
-                <div id="loginResult" className="red-text">{message}</div>
+                </div>
             </div>
         </div>
     );
