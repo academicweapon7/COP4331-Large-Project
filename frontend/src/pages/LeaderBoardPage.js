79c4960c60f44a8cf0ab725978ba17d7626d0c52
Matthew McCullough <matthewmccullough01@gmail.com>
2024-04-17 22:04:49 -0400

    LeaderBoard -> Leaderboard and loading table data

--- frontend/src/pages/LeaderBoardPage.js
+++ frontend/src/pages/LeaderBoardPage.js
@@ -3,7 +3,6 @@ import PageTitle from '../components/PageTitle';
 import Logo from '../components/Logo';
 import Leaderboard from '../components/Leaderboard';
 
-
 const LeaderboardPage = () =>
 {
     return(
