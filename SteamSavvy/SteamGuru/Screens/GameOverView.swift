f9f975fd1233ecae1ac2547df40b09138bb69395
Gabe Grassi <131699222+ggrassi09@users.noreply.github.com>
2024-04-24 17:31:27 -0400

    added call to the game over endpoint and updating display for high score

--- SteamSavvy/SteamGuru/Screens/GameOverView.swift
+++ SteamSavvy/SteamGuru/Screens/GameOverView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct GameOverView: View {
     @State private var showGameView = false
     @State private var showWelcomeView = false
+    @State private var highscore = 0
     @Binding var score: Int
     @Binding var username: String
    
 
     var body: some View {
         
-        NavigationStack{
+        doGameover(username: username, score: score)
+        
+       return NavigationStack{
             ZStack{
                 Color.primarycolor.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 VStack{
@@ -28,6 +31,13 @@ struct GameOverView: View {
                         .foregroundColor(.white)
                     
                     Text("Score: \(score)")
+                        .font(.body)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                    
+                    Text("High Score: \(highscore)")
                         .font(.body)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
@@ -58,7 +68,7 @@ struct GameOverView: View {
                    
                     .navigationBarBackButtonHidden(true)
                     .navigationDestination(isPresented: $showGameView){
-                        GameView(username: $username)
+                        GameView(username: $username, highscore: $highscore)
                     }
                     .navigationDestination(isPresented: $showWelcomeView){
                         WelcomeView()
@@ -67,6 +77,68 @@ struct GameOverView: View {
             }
         }
     }
+    
+    func doGameover(username: String, score: Int){
+        let gameoverEndpoint: String = "https://steamguru-77d4152ed074.herokuapp.com/api/gameover"
+        guard let gameoverURL = URL(string: gameoverEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        var gameoverUrlRequest = URLRequest(url: gameoverURL)
+        gameoverUrlRequest.httpMethod = "POST"
+        
+        let parameters: [String: Any] = [
+            "login": username,
+            "score": score
+        
+        ]
+        
+        let jsonGameover: Data
+        do {
+            jsonGameover = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            gameoverUrlRequest.httpBody = jsonGameover
+        } catch{
+            print("Error: cannot create JSON from login")
+            return
+        }
+        
+        gameoverUrlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        gameoverUrlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: gameoverUrlRequest){
+            (data, response, error) in
+            guard error == nil else {
+                print("error calling POST on /api/login")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            
+            do {
+                guard let receivedGameover = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] else {
+                    print("Could not get JSON from responseData as dictionary")
+                    return
+                }
+                print("The highscore is: " + receivedGameover.description)
+                
+                highscore = receivedGameover["highscore"] as! Int
+                
+                
+            } catch {
+                print("error parsing response from POST on /login")
+                return
+            }
+            
+            
+        }
+        task.resume()
+    }
+    
 }
 
 struct GameOverPreviewContainer : View {
